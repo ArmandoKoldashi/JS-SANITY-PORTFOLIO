d527231ff76ebf7dde7a691054eb5317152880cf
ArmandoKoldashi <145776904+ArmandoKoldashi@users.noreply.github.com>
2023-10-03 13:00:08 +0100

    test

--- schemas/index.ts
+++ schemas/index.ts
@@ -1,1 +1,18 @@
-export const schemaTypes = []
+import abouts from './abouts'
+import brands from './brands'
+import contact from './contact'
+import experiences from './experiences'
+import skills from './skills'
+import workExperience from './workExperience'
+import testimonials from './testimonials'
+import works from './works'
+
+export const schema = [abouts]
+export const schema = [testimonials]
+export const schemaTypes = [brands]
+export const schemaTypes = [contact]
+export const schemaTypes = [experiences]
+export const schemaTypes = [skills]
+export const schemaTypes = [workExperience]
+export const schemaTypes = [works]
+
